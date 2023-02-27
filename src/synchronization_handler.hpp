1df48e212e5342cef3e16235a40da445a381fd9b
Justus Stahlhut <justus.stahlhut@t-online.de>
2023-02-27 15:03:59 +0100

    stoneface

--- src/synchronization_handler.hpp
+++ src/synchronization_handler.hpp
@@ -25,30 +25,30 @@ private:
         
         bool is_game_over() {
             for (int i=0; i!=9; i+=3) {
-                if (field[i] == field[i+1] && field[i+1] == field[i+2]) {
+                if (field[0] != ' ' && field[i] == field[i+1] && field[i+1] == field[i+2]) {
                     is_won = field[i] == player;
                     return true;
                 }
             }
             
             for (int i=0; i!=3; i++) {
-                if (field[i] == field[i+3] && field[i+3] == field[i+6]) {
+                if (field[0] != ' ' && field[i] == field[i+3] && field[i+3] == field[i+6]) {
                     is_won = field[i] == player;
                     return true;
                 }
             }
             
-            if (field[0] == field[4] && field[4] == field[8]) {
+            if (field[0] != ' ' && field[0] == field[4] && field[4] == field[8]) {
                 is_won = field[0] == player;
                 return true;
             }
             
-            if (field[0] == field[4] && field[4] == field[8]) {
+            if (field[0] != ' ' && field[0] == field[4] && field[4] == field[8]) {
                 is_won = field[0] == player;
                 return true;
             }
             
-            if (field[2] == field[4] && field[4] == field[6]) {
+            if (field[0] != ' ' && field[2] == field[4] && field[4] == field[6]) {
                 is_won = field[2] == player;
                 return true;
             }
