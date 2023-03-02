b4372f4fd2c7b6bc91effd098ef7a204d56e74a6
Justus Stahlhut <justus.stahlhut@t-online.de>
2023-03-02 16:37:59 +0100

    waiting for move

--- src/synchronization_handler.cpp
+++ src/synchronization_handler.cpp
@@ -13,20 +13,23 @@ void SynchronizationHandler::play(char* challenger) {
     
     int sckt = network.get_chlg_sckt();
     int port = network.get_chlg_port();
-    char* buffer = new char[8]();
+    char* buffer = new char[8];
+    std::memset(buffer, 0, 8);
     bool receiving = true;
     
     std::thread recv_thread([this, sckt, &buffer, &receiving]() { network.receive_message(sckt, buffer, receiving); });
     
     network.send_message(sckt, challenger, "START", port);
     
-    while (*buffer == '\0') {
+    while (*buffer == '0') {
         network.send_message(sckt, challenger, "START", port);
         std::cout << "sent start" << std::endl;
         std::this_thread::sleep_for(std::chrono::milliseconds(300));
     }
     
-    buffer = new char[8]();
+    buffer = new char[8];
+    std::memset(buffer, 0, 8);
+    
     std::this_thread::sleep_for(std::chrono::milliseconds(50));
     
     std::cout << "Starting Game." << std::endl;
@@ -54,14 +57,15 @@ void SynchronizationHandler::play(char* challenger) {
             network.send_message(sckt, challenger, std::to_string(move).c_str(), port);
         } else {
             while (!ttt.is_move) {
-                if (buffer && std::strlen(buffer) == 0) {
+                if (*buffer == '0') {
                     std::this_thread::sleep_for(std::chrono::milliseconds(300));
                     continue;
                 }
                 short move = std::atoi(buffer);
                 std::cout << "not my move, played " << move << std::endl;
                 ttt.make_move(move);
-                buffer = new char[8]();
+                buffer = new char[8];
+                std::memset(buffer, 0, 8);
             }
         }
     }
