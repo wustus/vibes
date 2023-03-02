80ad9f00f4af01af59c7d485eab91cfc096be268
Justus Stahlhut <justus.stahlhut@t-online.de>
2023-03-02 17:07:36 +0100

    0xff

--- src/synchronization_handler.cpp
+++ src/synchronization_handler.cpp
@@ -14,14 +14,14 @@ void SynchronizationHandler::play(char* challenger) {
     int sckt = network.get_chlg_sckt();
     int port = network.get_chlg_port();
     char* buffer = new char[8];
-    std::memset(buffer, 0, 8);
+    std::memset(buffer, 0xFF, 8);
     bool receiving = true;
     
     std::thread recv_thread([this, sckt, &buffer, &receiving]() { network.receive_message(sckt, buffer, receiving); });
     
     network.send_message(sckt, challenger, "START", port);
     
-    while (*buffer == 0 && std::strcmp(buffer, "START") != 0) {
+    while (static_cast<unsigned char>(*buffer) == 0xFF && std::strcmp(buffer, "START") != 0) {
         network.send_message(sckt, challenger, "START", port);
         std::this_thread::sleep_for(std::chrono::milliseconds(300));
     }
@@ -54,14 +54,14 @@ void SynchronizationHandler::play(char* challenger) {
             network.send_message(sckt, challenger, std::to_string(move).c_str(), port);
         } else {
             while (!ttt.is_move) {
-                if (*buffer == 0) {
+                if (static_cast<unsigned char>(*buffer) == 0xFF) {
                     std::this_thread::sleep_for(std::chrono::milliseconds(300));
                     continue;
                 }
                 short move = std::atoi(buffer);
                 std::cout << "not my move, played " << move << std::endl;
                 ttt.make_move(move);
-                std::memset(buffer, 0, 8);
+                std::memset(buffer, 0xFF, 8);
             }
         }
     }
