44a29801acfd7cfd248678f0704b926497e7a3d5
Justus Stahlhut <justus.stahlhut@t-online.de>
2023-03-03 16:28:20 +0100

    twice?

--- src/synchronization_handler.cpp
+++ src/synchronization_handler.cpp
@@ -168,6 +168,7 @@ void SynchronizationHandler::determine_master() {
                 }
             }
         } else if (challenged) {
+            network.send_message(sckt, chlg_device, "ACC", CHLG_PORT);
             std::this_thread::sleep_for(std::chrono::milliseconds(300));
             network.send_message(sckt, chlg_device, "ACC", CHLG_PORT);
             challenger_found = true;
