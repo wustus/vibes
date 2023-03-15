a8f1bd0f8616c531e998e6b1a7612076cd6afd95
Justus Stahlhut <justus.stahlhut@t-online.de>
2023-03-15 14:34:44 +0100

    nvm

--- src/network.cpp
+++ src/network.cpp
@@ -265,8 +265,7 @@ bool Network::listen_for_ack(const char* addr, char* msg) {
         split_buffer_message(recv_addr, recv_msg, buffer_msg);
         
         if (std::strcmp(recv_addr, addr) == 0) {
-            uint16_t chksum;//static_cast<uint16_t>(strtoul(recv_msg, nullptr, 10));
-            std::memcpy(&chksum, recv_msg, sizeof(uint16_t));
+            uint16_t chksum = static_cast<uint16_t>(strtoul(recv_msg, nullptr, 10));
             
             if (chksum == calc_chksum) {
                 delete[] recv_addr;
@@ -299,7 +298,7 @@ void Network::send_ack(const char* addr, const char* msg) {
     
     uint16_t chksum = checksum((char*) msg);
     
-    std::snprintf(ack_msg, ack_msg_size, "%s::%d", net_config.address, htons(chksum));
+    std::snprintf(ack_msg, ack_msg_size, "%s::%d", net_config.address, chksum);
     
     if (sendto(ack_sckt, (void*)(intptr_t) ack_msg, ack_msg_size, 0, (struct sockaddr*) &dest_addr, sizeof(dest_addr)) < 0) {
         std::cerr << "Failed sending ACK for message: \n\t" << msg << std::endl << "Error: " << std::strerror(errno) << std::endl;
