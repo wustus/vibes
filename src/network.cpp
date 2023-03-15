c1b556906f09b2f54ee53ecf909f42f9d8009d93
Justus Stahlhut <justus.stahlhut@t-online.de>
2023-03-15 21:52:57 +0100

    remove logs

--- src/network.cpp
+++ src/network.cpp
@@ -821,8 +821,6 @@ void Network::ntp_server(uint32_t& start_time) {
             
             packet.start_time = htonl(start_time);
             
-            std::cout << "sending packet: " << packet.start_time << " " << htonl(packet.start_time) << std::endl;
-            
             if (sendto(ntp_sckt, &packet, NTP_PACKET_SIZE, 0, (struct sockaddr*) &src_addr, src_addr_len) < 0) {
                 std::cerr << "Error sending start time: " << std::strerror(errno) << std::endl;
             }
@@ -911,8 +909,6 @@ uint32_t Network::request_start_time(char* addr) {
         }
         
         packet = ntp_listener();
-        
-        std::cout << "receivingg packet: " << packet.start_time << " " << ntohl(packet.start_time) << std::endl;
         
         return ntohl(packet.start_time);
     }
