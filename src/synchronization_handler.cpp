52495418617e6439135e86cecd56683d29f21130
Justus Stahlhut <justus.stahlhut@t-online.de>
2023-03-20 17:22:35 +0100

    -1 win of waiting

--- src/synchronization_handler.cpp
+++ src/synchronization_handler.cpp
@@ -109,6 +109,7 @@ void SynchronizationHandler::determine_master() {
         
         if (challenger == nullptr) {
             std::cout << "Waiting for next Game..." << std::endl;
+            wins--;
             network.announce_status(nullptr, "WAIT", game_status);
             network.wait_for_challenge(game_status);
             std::cout << "Found Match" << std::endl;
